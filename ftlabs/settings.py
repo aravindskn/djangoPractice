88434cd604dbcacb9634447d1719551e244c046a
Aravind Sreekumar <aravind.innoventes@phonepe.com>
2020-07-07 20:16:40 +0530

    updated settings

--- ftlabs/settings.py
+++ ftlabs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rj=w(##=%jj&@(o$b4zdrya2f(!#yds(reri9+6auvvw=$6x&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ftlabstest.herokuapp.com']
 
 
 # Application definition
